1cfe9ab44a63f02ee1640080c9551ce8e50a38e3
Ikraam <ibndaanis@gmail.com>
2025-10-05 06:40:39 +0400

    netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
@@ -19,6 +20,7 @@ export default defineConfig({
         routesDirectory: "./routes" // Defaults to "routes", relative to srcDirectory
       }
     }),
-    viteReact()
+    viteReact(),
+    netlify()
   ]
 });
